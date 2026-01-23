7bae9b7ab05f5323acd983696c83b96e11b7218b
Pasquale Crispino <pasqualecrispino@MBP-di-Pasquale.station>
2026-01-23 19:01:53 +0100

    Fix: load global CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import "./index.css"; // 👈 QUESTA
