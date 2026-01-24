c6730f14336971f972f3bebbfa4a8d9f82d7d176
commtamo10-tech <commtamo10@gmail.com>
2026-01-24 12:38:58 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import NewDraft from "./pages/NewDraft";
 import DraftEditor from "./pages/DraftEditor";
-import Layout from "./components/Layout";
-import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/new" element={<NewDraft />} />
-            <Route path="/draft/:id" element={<DraftEditor />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/new" element={<NewDraft />} />
+        <Route path="/draft/:id" element={<DraftEditor />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
