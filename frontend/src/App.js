25def3be359f9e03d482247408836324835ae33d
commtamo10-tech <commtamo10@gmail.com>
2026-01-24 12:37:17 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import NewDraft from "./pages/NewDraft";
 import DraftEditor from "./pages/DraftEditor";
+import Layout from "./components/Layout";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          {/* 🔥 LOGIN ELIMINATA */}
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/new" element={<NewDraft />} />
-          <Route path="/draft/:id" element={<DraftEditor />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/new" element={<NewDraft />} />
+            <Route path="/draft/:id" element={<DraftEditor />} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </AuthProvider>
   );
